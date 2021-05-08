621c982d14d2f045a164520f629bcbac5b9ab304
adrian <adry.dabp@gmail.com>
2021-05-08 09:06:20 -0400

    Apollo client

--- src/App.js
+++ src/App.js
@@ -3,21 +3,25 @@ import {
   Switch,
   Route
 } from "react-router-dom";
+import { ApolloProvider } from '@apollo/client';
 import { Container } from 'semantic-ui-react';
 import Paths from './routes';
+import client from './apollo';
 import './App.css';
 
 const Routes = () => (Paths || []).map((item, idx) => <Route key={idx} exact={item.exact} path={item.path} component={item.component} />);
 
 const App = () => {
   return (
-    <Router>
-      <Container>
-        <Switch>
-          <Routes/>
-        </Switch>
-      </Container>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <Container>
+          <Switch>
+            <Routes/>
+          </Switch>
+        </Container>
+      </Router>
+    </ApolloProvider>
   );
 }
 
